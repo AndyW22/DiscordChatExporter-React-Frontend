b2304bcead57ce7b869f35ccc95c88bba338575b
Andy Waller <Andrew.waller24@outlook.com>
2022-07-30 17:17:48 +0100

    add START_ID

--- main.ts
+++ main.ts
@@ -1,20 +1,30 @@
 import 'dotenv/config';
 import fs from 'fs';
 import discord_log from './discord_log.json';
-import { DiscordLog } from './types';
+import { DiscordLog, Message } from './types';
 
-const MIN_DATE = process.env.MIN_DATE as string; // ISO date, ie 2020-01-01
+const START_DATE = process.env.MIN_DATE; // ISO date, ie 2020-01-01
+const START_ID = process.env.START_ID; // start ID, ie 667685634015055863
 
 export const main = async () => {
   const discordLog = discord_log as DiscordLog;
+  let currentMessages: Message[] = discordLog.messages;
+  if (START_DATE) {
+    const minimumDate = new Date(START_DATE).toISOString();
+    const filteredMessages = discordLog.messages.filter(
+      (message) => message.timestamp > minimumDate
+    );
+    currentMessages = filteredMessages;
+  }
+  if (START_ID) {
+    const startIdIndex = currentMessages.findIndex(
+      (message) => message.id === START_ID
+    );
+    currentMessages = currentMessages.slice(startIdIndex);
+  }
+  console.log(`Exporting ${currentMessages.length} messages.`);
 
-  const minimumDate = new Date(MIN_DATE).toISOString();
-  const filteredMessages = discordLog.messages.filter(
-    (message) => message.timestamp > minimumDate
-  );
-  console.log(`Exporting ${filteredMessages.length} messages.`);
-
-  const mappedMessages = discordLog.messages.map((message) => ({
+  const mappedMessages = currentMessages.map((message) => ({
     id: message.id,
     timestamp: message.timestamp,
     content: message.content,
